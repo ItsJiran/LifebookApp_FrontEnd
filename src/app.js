477c160e9eacf92ae8cae37cfca38b1a1287deee
ItsJiran <its.gilangramadhan@gmail.com>
2023-08-26 11:00:29 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+const App = () =>{
+    return (
+      <h2 class='text-5l font-sans font-bold underline'>
+          Welcome to  React App thats build using Webpack and Babel separately  
+      </h2>
+    )
+}
+
+export default App
